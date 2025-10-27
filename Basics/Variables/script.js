cc9f3a4e8c660b433757ad78c0dd90e8dad402b2
Balia077 <balaramd894@gmail.com>
2025-10-27 12:16:49 +0530

    Refactor variable declarations and comments in script.js for clarity

--- Basics/Variables/script.js
+++ Basics/Variables/script.js
@@ -3,3 +3,22 @@
 var username = "Balaram";
 console.log(username); // Balaram
 
+/* 
+var a;      //Declaration
+let a;      
+var a = 12; //Initialization
+let a = 12;
+const a = 12;
+*/
+
+// Var is window scoped
+// var is function scoped
+// var can be re-declared and updated
+
+// let is block scoped
+// let can be updated but not re-declared
+
+// const is block scoped
+// const cannot be updated or re-declared
+// const must be initialized during declaration
+
