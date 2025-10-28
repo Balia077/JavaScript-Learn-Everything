6a0c73c9ce49421dcac0cb965a59c6d164a08aa1
Balia077 <balaramd894@gmail.com>
2025-10-28 18:55:19 +0530

    Add initial HTML and JavaScript files for Primitive and Reference data types

---
+++ Basics/Datatypes/Primitive/script.js
@@ -0,0 +1,11 @@
+// Data Types
+
+// Primitive
+// Aisi sari values jinko copy karne par tumhe ek real or nayi copy mil jaye
+
+// String, number, bolean, null, undefined, symbol, bigInt
+
+let a = 12;
+
+let b = a;
+a = a + 2;
