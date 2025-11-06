5d76bf63ba0ea835d1504be2c1518724bcc0a90d
Balia077 <balaramd894@gmail.com>
2025-11-06 11:37:26 +0530

    Add initial HTML and JavaScript files for Operators demonstration

---
+++ Basics/Operators/script.js
@@ -0,0 +1,37 @@
+// Operators
+
+// Arithmatic Operator
+// + - / * % **
+
+console.log(1 + 2); // Addition --> 3
+console.log("Bal" + "ram"); // String Concatenation --> "Balram"
+
+console.log(10 - 5); // Substraction --> 5
+
+console.log(10 / 5); // Devision --> 2
+
+console.log(10 * 5); // Multiplication --> 50
+
+console.log(12 % 2); // Modulos (remainder) --> 0
+
+console.log(2 ** 3); // Power --> 8
+
+// Comparison Operators
+// == === != !== > < >= <=
+
+console.log(5 == "5"); // Equal to (value) --> true
+
+console.log(5 === "5"); // Equal to (value and type) --> false
+
+console.log(5 != "6"); // Not Equal to (value) --> true
+
+console.log(5 !== "5"); // Not Equal to (value and type) --> true
+
+console.log(5 > 3); // Greater than --> true
+
+console.log(3 < 5); // Less than --> true
+
+console.log(5 >= 5); // Greater than or equal to --> true
+
+console.log(3 <= 5); // Less than or equal to --> true
+
