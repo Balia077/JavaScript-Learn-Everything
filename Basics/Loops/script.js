68a7fd3657b6f661e7220e46ab03ec3b4ff39891
Balia077 <balaramd894@gmail.com>
2025-11-12 13:06:41 +0530

    Add initial HTML and JavaScript files for Loops demonstration

---
+++ Basics/Loops/script.js
@@ -0,0 +1,27 @@
+// Loops
+
+// for, while, do-while, forEach, for-in, for-of
+
+// for
+
+/* for(let i = 1; i <= 100; i++){
+    console.log(i);
+}
+ */
+
+// while
+
+/* let j = 1;
+while(j < 32){
+    console.log(j);
+    j++;
+} */
+
+// do-while
+
+let k = 1;
+
+do{
+    console.log(k);
+    k++;
+}while(k < 10);
