52e7f9c796b0df1c3d37b2ff33eff9999879c063
Balia077 <balaramd894@gmail.com>
2025-11-12 11:56:21 +0530

    Implement grading function and add test cases in script.js

--- Basics/Control Flow/script.js
+++ Basics/Control Flow/script.js
@@ -8,3 +8,12 @@ if(condition){
 }
 */
 
+function getVal(val){
+    if(val < 25) return 'F';
+    else if(val < 50) return 'C';
+    else if(val < 75) return 'B';
+    else return 'A';
+}
+
+console.log(getVal(80)); // A
+console.log(getVal(40)); // C
