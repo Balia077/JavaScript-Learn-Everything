0bc7d550d9a4fc53d506a9e7253ba0bd5850dee7
Balia077 <balaramd894@gmail.com>
2025-11-13 11:22:55 +0530

    Adding Functions declarations arguments & parameters

---
+++ Functions In JS/script.js
@@ -0,0 +1,31 @@
+// Functions
+
+// Declarations
+
+function Balia(){
+
+}
+
+// function Expression
+
+let fnc = function(){
+ 
+}
+
+// fat arrow Function
+
+let func = () => {
+
+};
+
+// Parameters & Arguments
+
+// Example
+
+function dance(value){
+    console.log(`${value} nach rha hai`);
+}
+
+dance("Ghoda"); //Argument
+dance("Balia"); //Argument
+
