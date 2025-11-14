bd621097509c5b849f3ce828077d05d5c99102b3
Balia077 <balaramd894@gmail.com>
2025-11-14 13:34:06 +0530

    Array Methods push pop shift unshift splice slice

--- Arrays/script.js
+++ Arrays/script.js
@@ -2,9 +2,42 @@
 
 // Declaration
 
-// let marks = [10, 20, 30, 40, 50];
+let marks = [10, 20, 30, 40, 50];
 
 // Access
 
-// marks[0];
+// marks[0]; 10
+
+// Modify Array
+
+marks[1] = 200;
+
+// Array Methods
+
+let arr = [1, 2, 3, 4, 5];
+
+// push()
+
+arr.push(7);    // [1, 2, 3, 4, 5, 7] -> 7 is added in last
+
+// pop()
+
+arr.pop();  //[1, 2, 3, 4, 5] -> 7 removed
+
+// shift()
+
+arr.shift();    //[2, 3, 4, 5] -> 1 removed
+
+// unshift()
+
+arr.unshift(0); //[0, 2, 3, 4, 5] -> 0 added in first
+
+// splice()
+
+arr.splice(1,2);   //[0, 4, 5] -> 2 & 3 removed
+
+// slice()
+
+let newarr = arr.slice(1,2);
+console.log(newarr);    //[0, 4] -> it make a copy of the sellected array elements of the main array and make a new array, it does not make any changes in the main array.
 
