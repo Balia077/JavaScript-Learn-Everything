9b39fba7e58e93c6c6b0003e0ac64cbe5ecc052a
Balia077 <balaramd894@gmail.com>
2025-11-15 12:50:54 +0530

    object declaration access nesting and deep access destructuring looping object copying

---
+++ Objects/script.js
@@ -0,0 +1,56 @@
+// Objects
+
+// Declaration
+
+let obj = {
+    name: "Balaram",
+    age: 19,
+};
+
+// Access
+
+console.log(obj.name);      //Balarma
+console.log(obj['age']);    //19
+
+// Nesting and deep access
+
+// Nesting
+
+const user = {
+    name: "Balia",
+    address: {
+        city: "Jajpur",
+        pin: 755043,
+        location: {
+            lat: 22.2,
+            lng: 77.4,
+        },
+    },
+};
+
+// Deep access
+
+console.log(user.address.location.lng);     //77.4
+
+// Object destructuring
+
+let {lat, lng} = user.address.location;
+console.log(lat, lng);
+
+// Looping
+
+let ob = {
+    name: "Balaram",
+    age: 19,
+    email: "test@test.com",
+};
+
+for(let key in ob){
+    console.log(ob[key]);
+}
+
+// Object Copying
+
+let obj2 = {...ob};
+console.log(obj2);      //{name: 'Balaram', age: 19, email: 'test@test.com'}
+
