2d18385f2f4af5b1a102a757a027a494f44173e7
Balia077 <balaramd894@gmail.com>
2025-11-19 19:39:52 +0530

    we make it

--- PHASE-2/Forms and Forms Validation/Practice/Email Password Validator/script.js
+++ PHASE-2/Forms and Forms Validation/Practice/Email Password Validator/script.js
@@ -4,8 +4,31 @@ let form = document.querySelector("form");
 
 form.addEventListener("submit", (dets)=>{
     dets.preventDefault();
+
+    document.querySelector("#emailError").textContent = "";
+    document.querySelector("#passwordError").textContent = "";
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
 
+    let emailAns = emailRegex.test(email.value);
+    let passwordAns = passwordRegex.test(password.value);
+
+    let isValid = true;
+
+    if(!emailAns){
+        document.querySelector("#emailError").textContent = "Email is Incorrect";
+        document.querySelector("#emailError").style.display = "initial";
+        isValid = false;
+    }
     
+    if(!passwordAns){
+        document.querySelector("#passwordError").textContent = "Password is Incorrect";
+        document.querySelector("#passwordError").style.display = "initial";
+        isValid = false;
+    }
+
+    if(isValid === true){
+        document.querySelector("#resultMessage").textContent = "Everything is Correct";
+    }
 });
