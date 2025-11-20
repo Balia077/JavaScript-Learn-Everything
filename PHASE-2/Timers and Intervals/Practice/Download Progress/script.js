875da508faea59d9464a4ee89f219eb6ff7d0da2
Balia077 <balaramd894@gmail.com>
2025-11-20 21:08:14 +0530

    I Make a download progress ui added pause and cancel button fully functional

---
+++ PHASE-2/Timers and Intervals/Practice/Download Progress/script.js
@@ -0,0 +1,63 @@
+let download = document.querySelector("#download");
+let controls = document.querySelector(".controls");
+let pau = document.querySelector(".pau");
+let can = document.querySelector(".can");
+let stats = document.querySelector(".stats");
+let dgb = document.querySelector(".dgb");
+let perc = document.querySelector(".perc");
+let pTrack = document.querySelector(".progress-track");
+let pBar = document.querySelector(".progress-bar");
+let sub = document.querySelector(".sub");
+let h3 = document.querySelector(".h3");
+
+let intvervalId;
+let count = 0;
+let flag = true;
+let seconds = 10;
+
+download.addEventListener("click", function () {
+    download.style.display = "none";
+    controls.style.display = "flex";
+    pTrack.style.display = "flex";
+    stats.style.display = "flex";
+    sub.style.display = "flex";
+    startInt();
+});
+
+function startInt() {
+    intvervalId = setInterval(function () {
+        if (count <= 99) {
+            count++;
+            pBar.style.width = `${count}%`;
+            dgb.textContent = `${count} GB`;
+            perc.textContent = `${count}%`;
+        }
+        else {
+            controls.style.display = "none";
+            h3.style.display = "flex"
+            h3.style.color = "#6ee7b7";
+            sub.style.display = "none";
+        }
+    }, (seconds * 1000) / 100);
+}
+
+pau.addEventListener("click", function () {
+    if (flag === true) {
+        clearInterval(intvervalId);
+        flag = false;
+    }
+    else {
+        startInt();
+        flag = true;
+    }
+});
+
+can.addEventListener("click", function () {
+    download.style.display = "flex";
+    controls.style.display = "none";
+    pTrack.style.display = "none";
+    stats.style.display = "none";
+    sub.style.display = "none";
+    flag = true;
+    count = 0;
+});
