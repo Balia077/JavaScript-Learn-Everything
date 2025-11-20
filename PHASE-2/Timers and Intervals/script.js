85c1d1f9dc9a961c26940513f8555ae501502967
Balia077 <balaramd894@gmail.com>
2025-11-20 19:24:57 +0530

    clearTimeout and clearInterval

--- PHASE-2/Timers and Intervals/script.js
+++ PHASE-2/Timers and Intervals/script.js
@@ -2,11 +2,27 @@
 
 //setTimeout
 
-setTimeout(function(){
+/* setTimeout(function(){
     console.log("hello world");
 },5000);
+ */
+//setInterval
 
 /* setInterval(function(){
     console.log("hello");
 },1000); */
 
+//clearTimeout
+/* 
+let tm = setTimeout(function(){
+    console.log("hey");
+}, 3000);
+
+clearTimeout(tm); */
+
+
+let st = setInterval(function(){
+    console.log("hey");
+}, 3000);
+
+clearInterval(st);
