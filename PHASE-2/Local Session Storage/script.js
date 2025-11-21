7bceb46bae01f5c5ffaa74571ef73eb3fe33bcb7
Balia077 <balaramd894@gmail.com>
2025-11-21 20:06:46 +0530

    setItem, getItem, removeItem & clear

--- PHASE-2/Local Session Storage/script.js
+++ PHASE-2/Local Session Storage/script.js
@@ -6,3 +6,31 @@
 
 // Cookies => chhota data hota hai jo aapke browser me store hota hai aur har request ke sath server ko bheja jata hai. Cookies ka size limited hota hai aur ye expiration date ke sath aate hain.
 
+// localStorage:-
+
+// Set item in localStorage
+localStorage.setItem("username", "balaram");
+
+// Get item from localStorage
+let val = localStorage.getItem("highScore");
+
+// Remove item from localStorage
+localStorage.removeItem("highScore");
+
+// Clear all items from localStorage
+localStorage.clear();
+
+// sessionStorage:-
+
+// Set item in sessionStorage
+sessionStorage.setItem("sessionName", "balaramSession");
+
+// Get item from sessionStorage
+let sessionVal = sessionStorage.getItem("sessionName");
+
+// Remove item from sessionStorage
+sessionStorage.removeItem("sessionName");
+
+// Clear all items from sessionStorage
+sessionStorage.clear();
+
