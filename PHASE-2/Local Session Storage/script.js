598c651f7c09e9e11f2e20a5cbbbe5348f696c45
Balia077 <balaramd894@gmail.com>
2025-11-21 20:24:42 +0530

    cookies, Storing array in localStorage. & Retrieving array from localStorage.

--- PHASE-2/Local Session Storage/script.js
+++ PHASE-2/Local Session Storage/script.js
@@ -34,3 +34,21 @@ sessionStorage.removeItem("sessionName");
 // Clear all items from sessionStorage
 sessionStorage.clear();
 
+// Cookies:-
+
+document.cookie = "name=balaram";
+let cookies = document.cookie;
+console.log(cookies);
+
+// To delete a cookie, set its expiration date to a past date
+document.cookie = "name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+// Note: localStorage and sessionStorage store data as strings. If you want to store objects or arrays, you need to use JSON.stringify() to convert them to strings before storing, and JSON.parse() to convert them back when retrieving.
+
+// Storing array in localStorage.
+localStorage.setItem("friends", JSON.stringify(["suraj", "ashish", "sumit"]));
+
+// Retrieving array from localStorage.
+let friends = JSON.parse(localStorage.getItem("friends"));
+console.log(friends); // Output: ["suraj", "ashish", "sumit"]
+
