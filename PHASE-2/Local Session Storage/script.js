71123678395837537ecbafe34f717bae465fa646
Balia077 <balaramd894@gmail.com>
2025-11-21 19:46:23 +0530

    Defination of localStorage, sessionStorage & Cookies

---
+++ PHASE-2/Local Session Storage/script.js
@@ -0,0 +1,8 @@
+// Local Storage, Session Storage and Cookies
+
+// localStorage => aapke browser ke andar data store karta hai browser band hone ke baad bhi data wahi rehta hai.
+
+// sessionStorage => aapke browser ke andar data store karta hai lekin jab browser band karte ho to data chala jata hai.
+
+// Cookies => chhota data hota hai jo aapke browser me store hota hai aur har request ke sath server ko bheja jata hai. Cookies ka size limited hota hai aur ye expiration date ke sath aate hain.
+
