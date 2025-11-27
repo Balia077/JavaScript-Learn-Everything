b601dbe82e3d52320f9389268c1c28f22a2458bf
Balia077 <balaramd894@gmail.com>
2025-11-27 18:01:05 +0530

    Implement drag-and-drop functionality and enhance task column styles in Kanban board

---
+++ Projects/Kanban Board/script.js
@@ -0,0 +1,27 @@
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach(task =>{
+    task.addEventListener("drag", (e) =>{
+
+    });
+});
+
+function addDragEvents(column){
+    column.addEventListener("dragenter", (e) => {
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
+    column.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+}
+
+
+
+addDragEvents(todo);
+addDragEvents(progress);
+addDragEvents(done);
